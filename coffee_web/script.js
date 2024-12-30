2118ab1cd64bab40a3af3b7d1123172b61af68c2
Duc anh <nguyenanh191025@gmail.com>
2024-12-31 03:00:43 +0700

    Update

--- coffee_web/script.js
+++ coffee_web/script.js
@@ -1,9 +1,39 @@
-const menuOpenButton = document.querySelector('#menu-open-button');
-const menuCloseButton = document.querySelector('#menu-close-button');
-const navMenu = document.querySelector('.nav-menu');
+const menuOpenButton = document.querySelector("#menu-open-button");
+const menuCloseButton = document.querySelector("#menu-close-button");
+const navMenu = document.querySelector(".nav-menu");
 
-menuOpenButton.addEventListener("click",() => {
-    navMenu.classList.toggle("show-mobile-menu");
+menuOpenButton.addEventListener("click", () => {
+  navMenu.classList.toggle("show-mobile-menu");
 });
 
-menuCloseButton.addEventListener("click",() => menuOpenButton.click())
+menuCloseButton.addEventListener("click", () => menuOpenButton.click());
+
+const swiper = new Swiper(".slider-wrapper", {
+  loop: true,
+  grabCursor: true,
+  spaceBetween: 25,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
